ef496ac9f24e4f5ea3ebd34f939c718ad741a310
hwadong119 <jeonghwayun0119@naver.com>
2023-09-30 23:47:24 +0900

    SearchBar 변경

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 import LeftNav from './LeftNav'
 import SearchBar from './SearchBar'
 import RightNav from './RightNav'
+import { BiArrowBack } from 'react-icons/bi'
+import { ImSearch } from 'react-icons/im'
+import { MdKeyboardVoice } from 'react-icons/md'
 
 const NavigationBar = () => {
+
+  const specialSearchBarRender = (
+    <div className='special_searchbar'>
+      <button>
+        <BiArrowBack size={25} />
+      </button>
+      <form>
+        <input type='text' name='search' placeholder='Search' />
+        <button type='submit'>
+          <ImSearch size={20} />
+        </button>
+      </form>
+      <button className='icon-container voiceIcon'>
+        <MdKeyboardVoice size={25} />
+      </button>
+    </div>
+  )
+
   return (
     <nav className='Navbar'>
       <LeftNav />

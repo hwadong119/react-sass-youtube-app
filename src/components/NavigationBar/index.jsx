fd903a41f21273066e3555437578d606a4bd86c6
hwadong119 <jeonghwayun0119@naver.com>
2023-09-30 23:58:56 +0900

    SearchContext 생성

--- src/components/NavigationBar/index.jsx
+++ src/components/NavigationBar/index.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import LeftNav from './LeftNav'
 import SearchBar from './SearchBar'
 import RightNav from './RightNav'
 import { BiArrowBack } from 'react-icons/bi'
 import { ImSearch } from 'react-icons/im'
 import { MdKeyboardVoice } from 'react-icons/md'
+import { SearchContext } from '../../context/SearchContext'
+import useWindowSize from '../../helpers/useWindowSize'
 
 const NavigationBar = () => {
 
+  const { width } = useWindowSize
+  const { showSpecialSearchBar, setShowSpecialSearchBar } = useContext(SearchContext);
+
   const specialSearchBarRender = (
     <div className='special_searchbar'>
-      <button>
+      <button onClick={() => setShowSpecialSearchBar(false)}>
         <BiArrowBack size={25} />
       </button>
       <form>
@@ -27,9 +32,15 @@ const NavigationBar = () => {
 
   return (
     <nav className='Navbar'>
-      <LeftNav />
-      <SearchBar />
-      <RightNav />
+      {width <= 640 && showSpecialSearchBar
+        ? specialSearchBarRender
+        : <>
+            <LeftNav />
+            <SearchBar />
+            <RightNav />
+          </>
+      }
+      
     </nav>
   )
 }

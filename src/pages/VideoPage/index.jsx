1e09c3144ba71a3292aad6a73340983380e7487f
hwadong119 <jeonghwayun0119@naver.com>
2023-10-02 14:52:43 +0900

    video page ui 생성

--- src/pages/VideoPage/index.jsx
+++ src/pages/VideoPage/index.jsx
@@ -2,6 +2,10 @@ import React, { useContext, useEffect } from 'react'
 import { useLocation, useParams } from 'react-router-dom'
 import YouTube from 'react-youtube';
 import { SidebarContext } from '../../context/SidebarContext';
+import { BiLike, BiDislike } from 'react-icons/bi'
+import { RiFlagLine, RiShareForwardLine } from 'react-icons/ri'
+import { MdPlaylistAdd } from 'react-icons/md'
+import dayjs from 'dayjs';
 
 const VideoPage = () => {
 
@@ -27,10 +31,102 @@ const VideoPage = () => {
     },
   };
 
-  return (
-    <div>
-      <YouTube className='youtube-player' videoId={videoId} onPlay={onPlayerReady} opts={opts} />
+  const videoHeaderMarkup = (
+    <div className='video_main_info'>
+      <div className='tags'>
+        {currentVideo?.snippet?.tags?.map((tag, i) =>(
+          <p className='tag' key={i}>#{tag}</p>
+        ))}
+      </div>
+      <h1>{currentVideo.snippet.title}</h1>
+      <div className='videoplayer_metadata'>
+        <span>
+          {currentVideo.extraInfo.viewCount} views
+        </span>
+        <span className='dot_separator'> &#8226; </span>
+        <span>
+          {dayjs(currentVideo.snippet.publishedAt).format('MMM D, YYYY')}
+        </span>
+      </div>
     </div>
+  )
+
+  return (
+    <section className='videoPage'>
+      <div className='columns_container'>
+        <div className='column column_1'>
+          <div className='youtube_player_container'>
+            <YouTube className='youtube-player' videoId={videoId} onPlay={onPlayerReady} opts={opts} />
+          </div>
+          <div className='videoplayer_info'>
+            {/* { videoaheaderMarkup } */}
+            {videoHeaderMarkup}
+            <div className='main_header_buttons'>
+              <div className='likes_container'>
+                <div className='likes'>
+                  <BiLike size={25} />
+                  <span>
+                    likes
+                  </span>
+                </div>
+                <div className='dislikes'>
+                  <BiDislike size={25} />
+                  <span>
+                    dislikes
+                  </span>
+                </div>
+              </div>
+              <div className='share'>
+                <RiShareForwardLine size={25} />
+                <span>
+                  SHARE
+                </span>
+              </div>
+              <div className='save'>
+                <MdPlaylistAdd size={25} />
+                <span>
+                  SAVE
+                </span>
+              </div>
+              <div className='report'>
+                <RiFlagLine size={25} />
+              </div>
+            </div>
+          </div>
+          <div className='channel_video_info'>
+            <div className='channel_data'>
+              <div className='channel_avatar'>
+                <img src={currentVideo.channelInfo.thumbnails.default.url} alt='avatar' />
+              </div>
+              <div className='channel_title'>
+                <a href='/'>
+                  {currentVideo.channelInfo.title}
+                </a>
+                <span>
+                  {currentVideo.channelInfo.subscriberCount} subscribers
+                </span>
+              </div>
+              <div className='channel_subscribe'>
+                <button>
+                  SUBSCRIBED
+                </button>
+              </div>
+            </div>
+            <div className='video_description'>
+              {currentVideo.snippet.description}
+            </div>
+          </div>
+          <div className='video_comments_container'>
+            <div className='video_comments_count'>
+              {currentVideo.extraInfo.commentCount} Comments
+            </div>
+            <div className='video_comments'>
+              {/* { videoCommentsMarkup} */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 

6604bc1583553114e24cdf557741017dde21a77f
hwadong119 <jeonghwayun0119@naver.com>
2023-10-02 13:56:03 +0900

    helper 함수들 생성

--- src/components/VideoCard/index.jsx
+++ src/components/VideoCard/index.jsx
@@ -2,8 +2,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { BiDotsVerticalRounded } from 'react-icons/bi'
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import formatViews from "../../helpers/formatViews";
+import formatTimeVideo from "../../helpers/formatTimeVideo";
 
 const VideoCard = ({ id, video, img, info, eInfo, channelInfo }) => {
+
+  dayjs.extend(relativeTime);
+  const time = dayjs(info.publishedAt).fromNow(true);
+  const views = formatViews(eInfo.viewCount);
+  const duration = formatTimeVideo(eInfo.duration);
+
   return (
     <div className="videoCard">
       <Link to={`/video/${id}`} state={{ ...video }}>
@@ -11,7 +21,7 @@ const VideoCard = ({ id, video, img, info, eInfo, channelInfo }) => {
           <img src={img} alt={info.title} />
           {eInfo.duration ? (
             <div className="video_duration">
-              <span>{eInfo.duration}</span>
+              <span>{duration}</span>
             </div>
           ) : null}
         </div>
@@ -35,9 +45,9 @@ const VideoCard = ({ id, video, img, info, eInfo, channelInfo }) => {
                 <div className="channelName">{info.channelTitle}</div>
               </Link>
               <div className="video_metadata">
-                <span>{eInfo.viewCount} views</span> &nbsp;
+                <span>{views} views</span> &nbsp;
                 <span className="dot_separator">&#8226;</span> &nbsp;
-                <span>{info.publishedAt}</span>
+                <span>{time} ago</span>
               </div>
             </div>
           </div>
